2d3034c58520388d2692b6f0f2a086d23ccdbddb
xNewz <pargornruasijan@gmail.com>
2021-11-28 01:52:54 +0700

    Update Plugin

--- 63030068/Plugin2.cs
+++ 63030068/Plugin2.cs
@@ -1,7 +1,7 @@
 using System;
 using PluginInterface;
 
-namespace Plugin2
+namespace Plugin_Pargorn
 {
 	/// <summary>
 	/// Plugin2
@@ -16,9 +16,9 @@ namespace Plugin2
 		}
 		
 		//Declarations of all our internal plugin variables
-		string myName = "Plugin2";
-		string myDescription = "Have Fun with this one! Click Away!";
-		string myAuthor = "Jon Dick";
+		string myName = "Plugin_Pargorn";
+		string myDescription = "Check you age !";
+		string myAuthor = "Pargorn Ruasijan";
 		string myVersion = "1.0.1";
 		IPluginHost myHost = null;
 		System.Windows.Forms.UserControl myMainInterface = new ctlMain();
@@ -40,51 +40,8 @@ namespace Plugin2
 		
 		}
 
-		/// <summary>
-		/// Host of the plugin.
-		/// </summary>
 		public IPluginHost Host
 		{
-			//This part is currently not really implemented
-			/*
-			Here's the scoop though... You can make the host program
-			Implement this interface... this essentially gives you the ability
-			to allow plugins to access some functionality of the host program.
-			
-			Example: an mp3 player.  If you had the IPluginHost interface as like:
-			
-			public interface IPluginHost
-			{
-				void Play(string FileName);
-				void Stop();			
-			}
-			
-			what you would do is when the plugin is loaded in the host (this would be
-			in like the PluginServices.cs file in the AddPlugin() method) you would 
-			set:
-				newPlugin.Host = this;
-				
-			this would give the plugin a reference to the host... now since the plugin
-			knows the host contains these methods, it can easily access them just like:
-			
-				this.Host.Play("C:\MyCoolMp3.mp3");
-				
-			and then they could go:
-			
-				this.Host.Stop();
-				
-			all this being from right inside the plugin!  I hope you get the point.  It 
-			just means that you can indeed make your plugins able to interact with the 
-			host program itself.  Let's face it.. It would be no fun if you couldn't do this,
-			because otherwise all the plugin is, is just standalone functionality running
-			inside the host program.. (of course there are cases when you can still accomplish
-			many things without needing to allow the plugin to play with the host... for example
-			you could have an spam filter, and have each plugin be a different filter... that would
-			be pretty simple to make plugins for...
-			
-			so anyhow, that is what the host thingy is all aboot, eh!	
-			
-			*/
 			get {return myHost;}
 			set	{myHost = value;}
 		}
--- 63030068/ctlMain.cs
+++ 63030068/ctlMain.cs
@@ -5,18 +5,22 @@ using System.Drawing;
 using System.Data;
 using System.Windows.Forms;
 
-namespace Plugin2
+namespace Plugin_Pargorn
 {
 	/// <summary>
 	/// Summary description for ctlMain.
 	/// </summary>
 	public class ctlMain : System.Windows.Forms.UserControl
-	{
-		private System.Windows.Forms.Button butMain;
-		/// <summary> 
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
+    {
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
 
 		public ctlMain()
 		{
@@ -49,56 +53,118 @@ namespace Plugin2
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.butMain = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// butMain
-			// 
-			this.butMain.BackColor = System.Drawing.Color.Gainsboro;
-			this.butMain.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.butMain.Location = new System.Drawing.Point(264, 224);
-			this.butMain.Name = "butMain";
-			this.butMain.Size = new System.Drawing.Size(88, 32);
-			this.butMain.TabIndex = 0;
-			this.butMain.Text = "&Click Me!";
-			this.butMain.Click += new System.EventHandler(this.butMain_Click);
-			// 
-			// ctlMain
-			// 
-			this.BackColor = System.Drawing.Color.White;
-			this.Controls.Add(this.butMain);
-			this.Name = "ctlMain";
-			this.Size = new System.Drawing.Size(664, 512);
-			this.Resize += new System.EventHandler(this.ctlMain_Resize);
-			this.ResumeLayout(false);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(131, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "คำนวน";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.on_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(102, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(14, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "กรุณาใส่ วัน/เดือน/ปี";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(60, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "พ.ศ.";
+            // 
+            // ctlMain
+            // 
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "ctlMain";
+            this.Size = new System.Drawing.Size(357, 236);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
-		#endregion
-		int newH = 0;
-		int newV = 0;
-			
-		Random rndH = new Random(20);
-		Random rndV = new Random(20);
-		
-		private void MoveButton()
-		{
-			newH = rndH.Next(this.Width - 40);
-			
-			
-			newV = rndV.Next(this.Height - 40);
-			
-			this.butMain.Top = newV;
-			this.butMain.Left = newH;
-		}
-		
-		private void butMain_Click(object sender, System.EventArgs e)
-		{
-			this.MoveButton();			
-		}
+        static string zodiac(int year)
+        {
+            string[] year_zodiac = {
+                "ปีมะเส็ง,ปีงูเล็ก", "ปีมะเมีย,ปีม้า", "ปีมะแม,ปีแพะ", "ปีวอก,ปีลิง", "ปีระกา,ปีไก่", "ปีจอ,ปีหมา",
+                "ปีกุน,ปีหมู", "ปีชวด,ปีหนู", "ปีฉลู,ปีวัว", "ปีขาล,ปีเสือ", "ปีเถาะ,ปีกระต่าย", "ปีมะโรง,ปีงูใหญ่"
+            };
+            return year_zodiac[year % 12];
+        }
+        private void on_Click(object sender, EventArgs e)
+        {
+            DateTime date_now = DateTime.Now;
+            DateTime date_input;
+            DateTime.TryParseExact(textBox1.Text, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out date_input);
 
-		private void ctlMain_Resize(object sender, System.EventArgs e)
-		{
-			this.MoveButton();
-		}
-	}
+            if (!DateTime.TryParseExact(textBox1.Text, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out date_input))
+                MessageBox.Show("คุณกรอก วัน/เดือน/ปี เกิดไม่ถูกต้อง !", "ล้มเหลว", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                int day, month, year;
+
+                int DaysInBdayMonth = DateTime.DaysInMonth(date_input.Year, date_input.Month);
+                int DaysRemain = date_now.Day + (DaysInBdayMonth - date_input.Day);
+
+                if (date_now.Month > date_input.Month)
+                {
+                    year = (date_now.Year + 543) - date_input.Year;
+                    month = date_now.Month - (date_input.Month + 1) + Math.Abs(DaysRemain / DaysInBdayMonth);
+                    day = (DaysRemain % DaysInBdayMonth + DaysInBdayMonth) % DaysInBdayMonth;
+                }
+                else if (date_now.Month == date_input.Month)
+                {
+                    if (date_now.Day >= date_input.Day)
+                    {
+                        year = (date_now.Year + 543) - date_input.Year;
+                        month = 0;
+                        day = date_now.Day - date_input.Day;
+                    }
+                    else
+                    {
+                        year = ((date_now.Year + 543) - 1) - date_input.Year;
+                        month = 11;
+                        day = DateTime.DaysInMonth(date_input.Year, date_input.Month) - (date_input.Day - date_now.Day);
+                    }
+                }
+                else
+                {
+                    year = ((date_now.Year + 543) - 1) - date_input.Year;
+                    month = date_now.Month + (11 - date_input.Month) + Math.Abs(DaysRemain / DaysInBdayMonth);
+                    day = (DaysRemain % DaysInBdayMonth + DaysInBdayMonth) % DaysInBdayMonth;
+                }
+
+                MessageBox.Show("อายุของคุณ " + year + " ปี " + month + " เดือน " + day + " วัน " + "\n" +
+                                "ปีนักษัตร : " + zodiac(date_input.Year), "ผลลัพท์", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+    #endregion
 }
